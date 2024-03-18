259c08cee49986239a089992d681636d52806a90
edaeren121 <51388399+edaeren121@users.noreply.github.com>
2024-03-18 02:43:12 +0000

    feat(areaLineChart): add area line chart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
+import AreaLineChart from "./AreaLineChart";
 import BarChart from "./BarChart";
+import LineChart from "./LineChart";
 import { useState, useEffect } from "react";
 import "./style.css";
-import LineChart from "./LineChart";
 
 function App() {
   const [fundData, setfundData] = useState(null);
@@ -23,6 +24,13 @@ function App() {
         <h2>Fund Performance</h2>
         <h1>Performance</h1>
       </header>
+      <div>
+        {fundData ? (
+          <AreaLineChart chartData={fundData} />
+        ) : (
+          <h1>Bar chart loading...</h1>
+        )}
+      </div>
       <div>
         {fundData ? (
           <BarChart chartData={fundData} />
---
+++ frontend/src/AreaLineChart.tsx
@@ -0,0 +1,116 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+);
+
+interface chartProp {
+  chartData: chartData;
+}
+
+interface chartData {
+  chartLabels: string[];
+  baseMonthlyPerformance: number[];
+  ltdPerformance: number[];
+  ytdPerformance: number[];
+}
+
+// This function is used to create the configs necessary to create a area line chart in react-charjs
+function AreaLineChart(props: chartProp) {
+  const chartXLabels = props.chartData.chartLabels;
+
+  const startDate = chartXLabels[0];
+  const endDate = chartXLabels[chartXLabels.length - 1];
+
+  const data = {
+    labels: props.chartData.chartLabels,
+    datasets: [
+      {
+        label: "Base monthly performance (%)",
+        fill: true,
+        data: props.chartData.baseMonthlyPerformance,
+        backgroundColor: "rgba(0, 255, 0, 0.4)",
+        borderColor: "green",
+        yAxisID: "y",
+      },
+      {
+        label: "Life to Date (%)",
+        fill: true,
+        data: props.chartData.ltdPerformance,
+        backgroundColor: "rgba(255, 0, 0, 0.4)",
+        borderColor: "red",
+        yAxisID: "y",
+      },
+      {
+        label: "Year to Date (%)",
+        fill: true,
+        data: props.chartData.ytdPerformance,
+        backgroundColor: "rgba(0, 0, 255, 0.4)",
+        borderColor: "blue",
+        yAxisID: "y",
+      },
+    ],
+  };
+
+  const options = {
+    maintainAçspectRatio: true,
+    responsive: true,
+    interaction: {
+      mode: "index" as const,
+      intersect: false,
+    },
+    stacked: false,
+    plugins: {
+      title: {
+        display: true,
+        text: "Fund Performance Details Chart",
+      },
+    },
+    scales: {
+      y: {
+        grid: {
+          color: "white",
+        },
+        type: "linear" as const,
+        display: true,
+        position: "left" as const,
+      },
+      x: {
+        grid: {
+          color: "white",
+        },
+      },
+    },
+  };
+
+  return (
+    <>
+      <h3>
+        Area line Chart from {startDate} - {endDate}
+      </h3>
+      <div id="areaLineChart">
+        <Line options={options} data={data} />
+      </div>
+    </>
+  );
+}
+
+export default AreaLineChart;

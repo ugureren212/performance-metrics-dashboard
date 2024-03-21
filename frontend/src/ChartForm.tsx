6077ffe331e700b12a201f5e60966080ace08898
Ugur Eren <ugureren212@gmail.com>
2024-03-21 04:55:17 +0000

    test commit

--- frontend/src/ChartForm.tsx
+++ frontend/src/ChartForm.tsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+
+function generateRandomGraphData() {
+  const graphData: number[] = [];
+  for (let i = 0; i < 12; i++) {
+    const randomNumber = Math.random() * 100;
+    graphData.push(Number(randomNumber.toFixed(2)));
+  }
+  return graphData;
+}
+
 function ChartForm() {
-  return <h3 style={{ color: "red" }}>some type of form</h3>;
+  const [graphData, setGraphData] = useState([1, 2, 3]);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    fetch("http://localhost:3030/fundData", {
+      method: "Post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(graphData),
+    });
+  }
+
+  return (
+    <>
+      <h3>some type of form</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="plotData">
+          Click button to add random data to graph:
+        </label>
+        <button
+          id="plotData"
+          onClick={() => {
+            setGraphData(generateRandomGraphData());
+          }}
+        >
+          Random plot data
+        </button>
+        <label htmlFor="deleteData">
+          Delete plotted data. Select from right panel:
+        </label>
+        <button id="deleteData">Delete plotted data</button>
+      </form>
+    </>
+  );
 }
 
 export default ChartForm;

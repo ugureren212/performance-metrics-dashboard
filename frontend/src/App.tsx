d1643cd1068038a478d0016d1f9243324fc12f54
Ugur Eren <ugureren212@gmail.com>
2024-04-16 17:51:28 +0100

    fix(mainContentFormat): fixed main content format using flexbox

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,32 +25,44 @@ function App() {
         {/* add start year and end year in heading */}
         <h2>Fund Performance between </h2>
       </header>
-      <div className="grid-container">
-        <div className="grid-item">
-          {" "}
-          {fundData ? (
-            <AreaLineChart chartData={fundData} />
-          ) : (
-            <h1>Bar chart loading...</h1>
-          )}
-        </div>
-        <div className="grid-item">
+
+      <div className="container">
+        <div className="full-width">
           <ChartForm />
         </div>
-        <div className="grid-item">
-          {fundData ? (
-            <BarChart chartData={fundData} />
-          ) : (
-            <h1>Bar chart loading...</h1>
-          )}
-        </div>
-        <div className="grid-item">
-          {" "}
-          {fundData ? (
-            <LineChart chartData={fundData} />
-          ) : (
-            <h1>Line chart loading...</h1>
-          )}
+        <div className="row">
+          <div className="container-box">
+            {" "}
+            {fundData ? (
+              <AreaLineChart chartData={fundData} />
+            ) : (
+              <h1>Bar chart loading...</h1>
+            )}
+          </div>
+          <div className="container-box">
+            {" "}
+            {fundData ? (
+              <BarChart chartData={fundData} />
+            ) : (
+              <h1>Bar chart loading...</h1>
+            )}
+          </div>
+          <div className="container-box">
+            {" "}
+            {fundData ? (
+              <LineChart chartData={fundData} />
+            ) : (
+              <h1>Line chart loading...</h1>
+            )}
+          </div>
+          <div className="container-box">
+            {" "}
+            {fundData ? (
+              <LineChart chartData={fundData} />
+            ) : (
+              <h1>Line chart loading...</h1>
+            )}
+          </div>
         </div>
       </div>
     </div>
